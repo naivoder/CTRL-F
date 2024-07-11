c535f5e276d7f72a9f6bfe75315dcccd73f5a9b4
credovian <cameron.redovian@r-dex.com>
2024-07-11 11:19:32 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# CTRL-F
+
+🚫 🚧 👷 Under Construction...
+
+CTRL-F: pairing convolution with transformer for image classification via multi-level feature cross-attention and representation learning fusion ([arXiv](https://www.arxiv.org/abs/2407.06673))
+
+![CTRL-F Network Diagram](network_diagram.png)
---
+++ network.py
@@ -0,0 +1,32 @@
+import torch
+import numpy as np
+
+
+class MBConvBlock(torch.nn.Module):
+    def __init__(self):
+        super(MBConvBlock, self).__init__()
+
+
+class SqueezeExcitationBlock(torch.nn.Module):
+    def __init__(self):
+        super(SqueezeExcitationBlock, self).__init__()
+
+
+class TransformerBlock(torch.nn.Module):
+    def __init__(self):
+        super(TransformerBlock, self).__init__()
+
+
+class MultiHeadSelfAttention(torch.nn.Module):
+    def __init__(self):
+        super(MultiHeadSelfAttention, self).__init__()
+
+
+class FeedForwardNetwork(torch.nn.Module):
+    def __init__(self):
+        super(FeedForwardNetwork, self).__init__()
+
+
+class CtrlF(torch.nn.Module):
+    def __init__(self):
+        super(CtrlF, self).__init__()
