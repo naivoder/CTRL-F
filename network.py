dac0913de4adb69d3053f5a6aa8a4d23c65325be
credovian <cameron.redovian@r-dex.com>
2024-07-11 11:28:18 -0400

    add stubbs for MFCA components and fusion strategies

--- network.py
+++ network.py
@@ -27,6 +27,26 @@ class FeedForwardNetwork(torch.nn.Module):
         super(FeedForwardNetwork, self).__init__()
 
 
+class CrossAttentionBlock(torch.nn.Module):
+    def __init__(self):
+        super(CrossAttentionBlock, self).__init__()
+
+
+class MFCATransformerModule(torch.nn.Module):
+    def __init__(self):
+        super(MFCATransformerModule, self).__init__()
+
+
 class CtrlF(torch.nn.Module):
     def __init__(self):
         super(CtrlF, self).__init__()
+
+
+class AdaptiveKnowledgeFusion(torch.nn.Module):
+    def __init__(self):
+        super(AdaptiveKnowledgeFusion, self).__init__()
+
+
+class CollaborativeKnowledgeFusion(torch.nn.Module):
+    def __init__(self):
+        super(CollaborativeKnowledgeFusion, self).__init__()
